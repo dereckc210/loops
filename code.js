322905f1b962d287e37a49c905969e8a9d5aa70f
dereckc210 <dereck.pirulin@icloud.com>
2025-04-14 13:17:54 -0400

    Update code.js

--- code.js
+++ code.js
@@ -2,14 +2,29 @@ const container = document.getElementByID("fruit")
 
 for(let i = 1 ; i <= 5 ; i++) {
   const p = document.createElement("p");
-  p.text.Content = "This is item #" + 1 ; 
+  p.text.Content = "This is item #" + i ; 
 container.appendChild(p);
 }
 
 let j = 1
 while(j <= 5) {
   const p = document.createElement(p);
-  p.textContent = "Whileloop item# + 
+  p.textContent = "Whileloop item# + j
     container.appendChild(p);
   j++;
 }
+
+
+for (let p = 1 ; p <= 20; p++) {
+  const p = document.createElement("p");
+  p.textContent = i;
+
+  if (p % 2 == 0) {
+    p.style.color = "blue";
+  } else {
+    p.style.color = "red";
+  }
+
+  container.appendChild(p);
+}
+

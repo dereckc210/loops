4a6a4dc9e5b4179a00c870a5d1a3a47b2c62cee1
dereckc210 <dereck.pirulin@icloud.com>
2025-04-14 13:11:04 -0400

    Update and rename code.jss to code.js

---
+++ code.js
@@ -0,0 +1,15 @@
+const container = document.getElementByID("fruit")
+
+for(let i = 1 ; i <= 5 ; i++) {
+  const p = document.createElement("p");
+  p.text.Content = "This is item #" + 1 ; 
+container.appendChild(p);
+}
+
+let j = 1
+while(j <= 5) {
+  const p = document.createElement(p);
+  p.textContent = "Whileloop item# + 
+    container.appendChild(p);
+  j++;
+}
